2d8059818ae97b47521fcddbd7247fe96f2d194c
unknown <leviettrung.social@gmail.com>
2022-06-19 21:49:39 +0700

    remove Home Link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,10 +6,9 @@ function SideBar() {
                 <div className="avt-box">
                     <img className="avt" src="https://profile.trug.cf/img/avt.jpg" alt="Avatar"/>
                 </div>
-                <Link to="" style={{ fontWeight: "bold" }}>
+                <Link to="./" style={{ fontWeight: "bold" }}>
                     Hi! Im Việt Trung
                 </Link>
-                <Link to="./">Home</Link>
                 <Link to="./about">About</Link>
                 <Link to="./contact">Contact</Link>
             </div>
